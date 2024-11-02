c6474fd5ef085f6975617fb898335cd8820139e5
Matt Leary <33221468+mleary@users.noreply.github.com>
2024-11-01 22:30:55 -0400

    update anthropic models, add cladue to chat

--- sections/chat.py
+++ sections/chat.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from openai import AzureOpenAI
+from anthropic import Anthropic
 
 from utils.llm_helpers import setup_azure_openai_client, setup_anthropic_client
 from config import CHAT_MATT_PROMPT, CHAT_USER_PROMPT
@@ -13,7 +14,7 @@ load_dotenv()
 # Constants
 MODEL_PROVIDERS = ["Azure OpenAI", 'Anthropic']
 AZURE_OPENAI_MODELS = ["gpt-4o-mini", "gpt-4o"]
-ANTHROPIC_MODELS = ["claude-3-5-sonnet-20240620", "claude-3-haiku-20240307"]
+ANTHROPIC_MODELS = ["claude-3-haiku-20240307", "claude-3-5-sonnet-20241022",]
 
 def initialize_session_state(GREETING):
     """
@@ -34,8 +35,10 @@ def set_system_prompt():
     else:
         return CHAT_USER_PROMPT
 
+
 def set_greeting():
     return f"Hi {st.session_state['username'].capitalize()}, how can I help you today?"
+
 
 def render_sidebar():
     """
@@ -63,30 +66,35 @@ def render():
     initialize_session_state(GREETING)
     system_prompt = set_system_prompt()
     client = setup_azure_openai_client()
-    client_claude = setup_anthropic_client
+    client_claude = setup_anthropic_client()
 
     for msg in st.session_state.messages:
         st.chat_message(msg["role"]).write(msg["content"])
     
     # Input for new messages
     if prompt := st.chat_input():
-        st.session_state.messages.append({"role": "user", "content": system_prompt}) 
+        #st.session_state.messages.append({"role": "user", "content": system_prompt}) 
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.chat_message("user").write(prompt)
         if st.session_state.model_provider == "Azure OpenAI":
             response = client.chat.completions.create(model=st.session_state.model_name, messages=st.session_state.messages)
+            msg = response.choices[0].message.content
         elif st.session_state.model_provider == "Anthropic":
-            response = client_claude.messages.create(model=st.session_state.model_name,
-            messages=[{"role": m["role"], "content": m["content"]}for m in st.session_state.messages]
-            )
-        msg = response.choices[0].message.content
+            print("Messages being sent:", st.session_state.messages)  # Debug print
+            response = client_claude.messages.create(
+                model=st.session_state.model_name,
+                #messages=[{"role": m["role"], "content": m["content"]}for m in st.session_state.messages],
+                messages=st.session_state.messages,
+                max_tokens=1000)
+            msg = response.content[0].text
+        
         st.session_state.messages.append({"role": "assistant", "content": msg})
         st.chat_message("assistant").write(msg)
      
     # Button to clear chat history
     if st.button("Clear Chat History"):
 
-        st.session_state["messages"] = [{"role": "system", "content": GREETING}]
+        st.session_state["messages"] = [{"role": "assistant", "content": GREETING}]
         st.rerun()
         
 
